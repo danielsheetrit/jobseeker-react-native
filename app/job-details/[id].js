02c2e831bb4cb3cde50913d9dfb9f1483fe56214
danielsheetrit <danileshitrit022@gmail.com>
2023-04-03 17:05:22 +0300

    add guard before showing data

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -52,17 +52,19 @@ function JobDetails() {
         refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
       >
         <FetchCmpWrapper loading={loading} error={error}>
-          <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
-            <Company
-              companyLogo={data[0].employer_logo}
-              jobTitle={data[0].job_title}
-              companyName={data[0].employer_name}
-              location={data[0].job_country}
-            />
-            <JobTabs tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab} />
+          {data.length > 0 && (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+              <JobTabs tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab} />
 
-            <DisplayCmpJunction activeTab={activeTab} tabs={tabs} data={data[0]} />
-          </View>
+              <DisplayCmpJunction activeTab={activeTab} tabs={tabs} data={data[0]} />
+            </View>
+          )}
         </FetchCmpWrapper>
       </ScrollView>
 
